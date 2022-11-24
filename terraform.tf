e59e58672cde8c1ed0a2127f9fb602d8bccfab9d
Ulagu9298 <110220579+Ulagu9298@users.noreply.github.com>
2022-11-24 17:58:52 +0530

    Update terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "demo_instance" {
+   
+    ami = "ami-08c40ec9ead489470"
+    instance_type = "t2.micro"
+    
+    tags = {
+        Name = "Terraform-Now"
+        
+          }
+}
